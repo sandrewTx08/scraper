02691f07a787621d3204cfbab1a5b0f9f5bc8fd0
sandrewtx08 <sandrewtx08@protonmail.com>
2022-07-25 17:34:02 -0300

    feat: class

--- src/scraper.ts
+++ src/scraper.ts
@@ -24,35 +24,45 @@ export type Configurations = {
   request: AxiosRequestConfig;
 };
 
-export function request<
+export class Scraper<
   T,
   F extends <R>($: CheerioAPI) => Cheerio<R>,
-  D extends { [KD in keyof T]: F }
->(
-  strategy: D,
-  options: Configurations
-): Promise<{ [KR in keyof D]: ReturnType<D[KR]> }[]> {
-  function parse(html: string) {
+  D extends { [KD in keyof T]: F },
+  R extends { [KR in keyof D]: ReturnType<D[KR]> }
+> {
+  constructor(
+    public readonly strategy: D,
+    public readonly options: Configurations
+  ) {}
+
+  request(): Promise<R[]> {
+    const data = [];
+
+    for (
+      let i = 0;
+      i < (this.options.url instanceof Array ? this.options.url.length : 1);
+      i++
+    ) {
+      data[i] = this.options.session
+        .request(this.options.request)
+        .then((response) => this.parse(response.data));
+    }
+
+    return Promise.all(data);
+  }
+
+  parse(html: string): R {
     const $page = load(html);
     const object = Object();
 
-    Object.keys(strategy).map((key) => {
+    Object.keys(this.strategy).map((key) => {
       const data = [];
-      const callback = strategy[<keyof T>key];
-      const d = callback($page);
-      data.push(d);
+      const callback = this.strategy[<keyof T>key];
+      const callback_data = callback($page);
+      data.push(callback_data);
       object[key] = data;
     });
 
     return object;
   }
-
-  const data = [];
-  for (let i = 0; i < 1; i++) {
-    data[i] = options.session
-      .request(options.request)
-      .then((response) => parse(response.data));
-  }
-
-  return Promise.all(data);
 }
