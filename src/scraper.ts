8aec8a005a94c27dec6687e97687a368fe4e2221
sandrewtx08 <sandrewtx08@protonmail.com>
2022-08-18 09:18:08 -0300

    refactor: modules

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,120 +1,13 @@
 import axios, { AxiosRequestConfig } from "axios";
-import { load } from "cheerio";
-import express, { Express } from "express";
-import { Cheerio, CheerioAPI } from "cheerio";
-
-type ModeCallback = ($: CheerioAPI) => Cheerio<any>;
-
-type ModeObject = { [x: string]: ModeCallback };
-
-type ModeArray = ModeCallback[];
-
-type ModesReturn<T extends Modes> = T extends ModeObject ? { [K in keyof T]:
-  ReturnType<T[K]> extends Cheerio<any> ? ReturnType<T[K]> : never }
-  : T extends ModeCallback ?
-  ReturnType<T> extends Cheerio<any> ? ReturnType<T> : never
-  : T extends (infer I extends ModeCallback)[] ?
-  ReturnType<I>[] extends Cheerio<any>[] ? ReturnType<I>[] : never
-  : never;
-
-type Modes = ModeObject | ModeCallback | ModeArray;
-
-interface IModeClass {
-  parser(html: string): any
-  createExpress(request: Function, hostname: string): Express
-}
+import { ModeArrayClass, ModeCallbackClass, ModeObjectClass } from "./parse";
+import { ModeArray, ModeCallback, ModeObject, Modes, ModesReturn } from "./types";
 
 function createScraper<Mode extends Modes>(mode: Mode) {
-  const ModeClass =
-    mode instanceof Array
-      ? class ModeArrayClass implements IModeClass {
-        constructor(public mode: ModeArray) { }
-
-        parser(html: string): ModesReturn<ModeArray> {
-          const $page = load(html);
-          return this.mode.map((callback) => callback($page));
-        }
-
-        createExpress(request: Function, hostname: string) {
-          const app = express();
-
-          return app.use((req, res) => {
-            const url = hostname + req.url.slice(1);
-
-            try {
-              request(url, (result: ModesReturn<ModeArray>) => {
-                res.json(result.map((data) => data.toArray()));
-              });
-            } catch (error: any) {
-              res.json(error.message);
-            }
-          });
-        }
-      }
-      : typeof mode === "function"
-        ? class ModeCallbackClass implements IModeClass {
-          constructor(private mode: ModeCallback) { }
-
-          parser(html: string): ModesReturn<ModeCallback> {
-            const $page = load(html);
-            return this.mode($page);
-          }
-
-          createExpress(request: Function, hostname: string) {
-            const app = express();
-
-            return app.use((req, res) => {
-              const url = hostname + req.url.slice(1);
-
-              try {
-                request(url, (result: ModesReturn<ModeCallback>) => {
-                  res.json(result.toArray())
-                });
-              } catch (error: any) {
-                res.json(error.message);
-              }
-            });
-          }
-        }
-        : class ModeObjectClass implements IModeClass {
-          constructor(private mode: ModeObject) { }
-
-          parser(html: string): ModesReturn<ModeObject> {
-            const $page = load(html);
-            const object = Object();
-
-            Object.keys(mode).forEach((key) => {
-              const callback = this.mode[key];
-              object[key] = callback($page);
-            });
-
-            return object;
-          }
-
-          createExpress(request: Function, hostname: string) {
-            const app = express();
-
-            return app.use((req, res) => {
-              const url = hostname + req.url.slice(1);
-
-              try {
-                request(url, (result: ModesReturn<ModeObject>) => {
-                  const object = Object();
-
-                  Object.keys(result).forEach((key) => {
-                    object[key] = result[key].toArray();
-                  });
-
-                  res.json(object);
-                });
-              } catch (error: any) {
-                res.json(error.message);
-              }
-            });
-          }
-        };
-
-  const modeClass = new ModeClass(<any>mode);
+  const modeClass = new (mode instanceof Array
+    ? ModeArrayClass
+    : typeof mode === "function"
+      ? ModeCallbackClass
+      : ModeObjectClass)(<any>mode);
 
   function staticPage(config: AxiosRequestConfig<any>): Promise<ModesReturn<Mode>>;
   function staticPage(config: AxiosRequestConfig<any>, callback: (result: ModesReturn<Mode>) => void): void;
---
+++ src/types.ts
@@ -0,0 +1,24 @@
+
+import { Cheerio, CheerioAPI } from "cheerio";
+import { Express } from "express";
+
+export interface IModeClass {
+    parser(html: string): any
+    createExpress(request: Function, hostname: string): Express
+}
+
+export type ModeCallback = ($: CheerioAPI) => Cheerio<any>;
+
+export type ModeObject = { [x: string]: ModeCallback };
+
+export type ModeArray = ModeCallback[];
+
+export type ModesReturn<T extends Modes> = T extends ModeObject ? { [K in keyof T]:
+    ReturnType<T[K]> extends Cheerio<any> ? ReturnType<T[K]> : never }
+    : T extends ModeCallback ?
+    ReturnType<T> extends Cheerio<any> ? ReturnType<T> : never
+    : T extends (infer I extends ModeCallback)[] ?
+    ReturnType<I>[] extends Cheerio<any>[] ? ReturnType<I>[] : never
+    : never;
+
+export type Modes = ModeObject | ModeCallback | ModeArray;
