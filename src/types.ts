7ab251382937286d0502f5b087237130f2d59150
sandrewtx08 <sandrewtx08@protonmail.com>
2022-08-29 16:52:02 -0300

    refactor: types

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,33 @@
-
 import { Cheerio, CheerioAPI } from "cheerio";
-import { Express } from "express";
+import { Router } from "express";
 
 export interface IModeClass<T extends Modes> {
-    parser(html: string): ModesReturn<T>
-    createExpress(request: Function, hostname: string): Express
+  parser(html: string): ModesReturn<T>;
+  createRouter(request: Function, hostname: string): Router;
 }
 
-export type ModeCallback = ($: CheerioAPI) => Cheerio<any>;
+export type Result = Cheerio<any>;
+
+export type ModeCallback = ($: CheerioAPI) => Result;
 
 export type ModeObject = { [x: string]: ModeCallback };
 
 export type ModeArray = ModeCallback[];
 
-export type ModesReturn<T extends Modes> = T extends ModeObject ? { [K in keyof T]:
-    ReturnType<T[K]> extends Cheerio<any> ? ReturnType<T[K]> : never }
-    : T extends ModeCallback ?
-    ReturnType<T> extends Cheerio<any> ? ReturnType<T> : never
-    : T extends (infer I extends ModeCallback)[] ?
-    ReturnType<I>[] extends Cheerio<any>[] ? ReturnType<I>[] : never
-    : never;
+export type ModesReturn<T extends Modes> = T extends ModeObject
+  ? {
+      [K in keyof T]: ReturnType<T[K]> extends Result
+        ? ReturnType<T[K]>
+        : never;
+    }
+  : T extends ModeCallback
+  ? ReturnType<T> extends Result
+    ? ReturnType<T>
+    : never
+  : T extends (infer I extends ModeCallback)[]
+  ? ReturnType<I>[] extends Result[]
+    ? ReturnType<I>[]
+    : never
+  : never;
 
 export type Modes = ModeObject | ModeCallback | ModeArray;
